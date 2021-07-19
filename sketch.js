700bd57054b65ccaf0e42d6715fddc2c39ced5b4
vansh suthar <78260154+vansh46@users.noreply.github.com>
2021-07-19 21:27:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw(){
 
 }
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
     var responseJSON = await response.json();
 
     console.log(responseJSON);
@@ -45,43 +45,16 @@ async function getBackgroundImg(){
 
     console.log(datetime+"  " +hour);
 
-    if(hour>=04 && hour<=06 ){
-       
+    if(hour>=04 && hour<=06){
         bg = "sunrise1.png";
-    }else if(hour>=06 && hour<=08 ){
-        
+    }else if(hour>=06 && hour<=08){
         bg = "sunrise2.png";
-    }else if(hour>=08 && hour<=11 ){
-        
-        bg = "sunrise3.png";
-    }else if(hour>=11 && hour<=13){
-       
-        bg = "sunrise4.png";
-    }else if(hour>=13 && hour<=15){
-        
-        bg = "sunrise5.png";
-    }else if(hour>=15 && hour<=17 ){
-        
-        bg = "sunrise6.png";
-    }else if(hour>=17 && hour<=18 ){
-        
-        bg = "sunset7.png";
-    }else if(hour>=18 && hour<=20 ){
-        
-        bg = "sunset8.png";
-    }else if(hour>=20 && hour<=23 ){
-       
-        bg = "sunset9.png";
     }else if(hour>=23 && hour==0){
-       
         bg = "sunset10.png";
-    }else if(hour==0 && hour<=03){
-       
+    }else if(hour>=0 && hour==03){
         bg = "sunset11.png";
     }else{
-        
         bg = "sunset12.png";
-  
     }
 backgroundImg = loadImage(bg);
 
